412af954b8a46d7937678d0ed14a459ad8abc313
mo24ali <alichere1234@gmail.com>
2026-04-17 20:06:13 +0100

    added the blog post policy

---
+++ app/Policies/BlogPostPolicy.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\BlogPost;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class BlogPostPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, BlogPost $blogPost): bool
+    {
+        if ($blogPost->visibility === 'public' && $blogPost->moderation_status === 'approved') return true;
+        return $user->id === $blogPost->author_id;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, BlogPost $blogPost): bool
+    {
+        return $user->id === $blogPost->author_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, BlogPost $blogPost): bool
+    {
+        return $user->id === $blogPost->author_id;
+    }
+
+    public function restore(User $user, BlogPost $blogPost): bool
+    {
+        return false;
+    }
+
+    public function forceDelete(User $user, BlogPost $blogPost): bool
+    {
+        return false;
+    }
+}
